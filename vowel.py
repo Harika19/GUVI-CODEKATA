3890f57d8b87125b04d842497dbc4703d819bfa1
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-03-25 22:43:13 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+ch=str(input())
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
+elif ch.isalpha():
+    print("Consonant")
+else:
+    print("Invallid")
