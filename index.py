60e143141a3c5fe7167a1d64ae33f7d7a8157c84
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-03-28 17:56:17 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=0
+t=0
 b=[]
 for i in range(n):
-    if (a[i]==i):
-        c=c+1
+    if a[i]==i:
+        t+=1
         b.append(i)
-
-if (c!=0):
+if t!=0:
     for j in b:
-        if (j!=b[-1]):
+        if j!=b[-1]:
             print(j,end=' ')
         else:
             print(j)
