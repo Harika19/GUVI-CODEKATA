9fd38d90dcfc1e8bfe0549887b68daf1c9734ae6
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-03-28 14:23:20 +0530

    Create repeatedsorting.py

---
+++ repeatedsorting.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=[]
+for x in a:
+    if x not in b:
+        b.append(x)
+    else:
+        c.append(x)
+d=set(c)
+c=list(d)
+c.sort()
+for i in c:
+    print(i,end=' ')
