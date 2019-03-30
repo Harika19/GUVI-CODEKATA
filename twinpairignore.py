8f0780887a002b91a2f6c19093c7af54f2caa04f
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-03-30 17:05:27 +0530

    Update twinpairignore.py

--- twinpairignore.py
+++ twinpairignore.py
@@ -1,7 +1,12 @@
 n=int(input())
 a=list(map(int,input().split()))
 a.sort()
+c=0
 for i in range(0,n-1,2):
     if(a[i]!=a[i+1]):
         print(a[i])
+        c=c+1
         break
+if(c==0):
+    print(a[-1])
+
