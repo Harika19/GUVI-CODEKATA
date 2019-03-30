9b774992cec92b3930435ab809ed32b698ade266
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-03-30 16:37:55 +0530

    Create removevower.py

---
+++ removevower.py
@@ -0,0 +1,12 @@
+#remove vowel and reverse a string
+i=str(input())
+k=list(i)
+r=0
+for ch in k:
+    if (ch == 'A' or ch == 'a' or ch == 'E' or ch == 'e' or ch == 'I'
+            or ch == 'i' or ch == 'O' or ch == 'o' or ch == 'U' or ch == 'u'):
+        k.remove(ch)
+f=k[::-1]
+s = ''.join(f)
+print(s)
+
