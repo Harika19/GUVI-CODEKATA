ab74becdf77add17b693f6cd34698cb3b1b6d2b4
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-04-21 12:37:59 +0530

    Update mimindexing.py

--- mimindexing.py
+++ mimindexing.py
@@ -1,11 +1,9 @@
+
 n,q=map(int,input().split())
 a=list(map(int,input().split()))
 x=[]
 for i in range(q):
     u,v=map(int,input().split())
     x.append(min(a[u-1:v]))
-for d in range(len(x)):
-    if d!=len(x)-1:
-        print(x[d],end='\n')
-    else:
-        print(x[d],end='')
+for d in range(0,len(x)):
+    print(x[d])
