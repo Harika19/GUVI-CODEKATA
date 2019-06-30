7ef5adc74c085456bf7e3668a9e373d70add4c17
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-06-30 16:20:12 +0530

    Create xsum.py

---
+++ xsum.py
@@ -0,0 +1,16 @@
+def sums(n):
+    s=0
+    r=0
+    while(n):
+        r=n%10
+        s=s+r
+        n=int(n/10)
+    return s
+def calss(n):
+    for i in range(n+1):
+        if(i+sums(i)==n):
+            return i      
+    return -1
+n=int(input())
+print("1",end='\n')
+print(calss(n))    
