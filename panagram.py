cb7cbd755697190fd43c3e4a405de817aa02d1ec
Harika19 <36102187+Harika19@users.noreply.github.com>
2019-07-06 09:00:25 +0530

    Update panagram.py

--- panagram.py
+++ panagram.py
@@ -1,12 +1,1 @@
-s=str(input())
-l=[]
-q=' abcdefghijklmnopqrstuvwxyz'
-for i in s:
-    if i not in l:
-        l.append(i)       
-l.sort()
-u=''.join(l)
-if(u==q):
-    print("yes")
-else:
-    print("no")        
+
